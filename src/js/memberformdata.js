45c9bd87b333e842d54cd52e10cb3f503429f699
Tripoli80 <Tripoli80@gmail.com>
2022-08-01 18:18:24 +0200

    бек старт

--- src/js/memberformdata.js
+++ src/js/memberformdata.js
@@ -1,4 +1,4 @@
-console.log('xsaxsa');
+// console.log('xsaxsa');
 
 const refs = {
   name: document.querySelector('#name'),
@@ -6,6 +6,12 @@ const refs = {
   service: document.querySelector('#service'),
   comment: document.querySelector('#comment'),
 };
+const dataToCrm = {};
+
+const send = document.querySelector('.form__btn');
+console.log('object', send);
+send.addEventListener('click', sendData);
+
 const serviceBtns = document.querySelectorAll('.services__btn');
 //записываю данные в форму относительно нажатой кнопки
 for (const iterator of serviceBtns) {
@@ -40,6 +46,64 @@ function readLocal() {
   for (const key in data) {
     if (Object.hasOwnProperty.call(data, key)) {
       refs[key].value = data[key];
+      dataToCrm[key] = data[key];
     }
   }
+}
+
+function sendData(e) {
+  e.preventDefault();
+
+  console.log('insidesenddata');
+
+  if (!validator.isMobilePhone(dataToCrm.tel, ['uk-UA'])) {
+    alert('Not valid Phone');
+    console.log(
+      'Not valid Phone ',
+      validator.isMobilePhone(data.tel, ['uk-UA'])
+    );
+    // refs.telAfter.classList.toggle('mymodal-form__box-tel-active');
+    return;
+  }
+  dataToCrm.url = document.URL;
+  dataToCrm.name_form = 'Консультация сайт';
+  dataToCrm.titel = 'Консультация сайт';
+
+  let form_data = new FormData();
+
+  for (var key in dataToCrm) {
+    form_data.append(key, dataToCrm[key]);
+
+    console.log(key, '---', dataToCrm[key]);
+  }
+  // console.log(`Отправка данных ${form_data.values}`);
+  const url = 'https://dev.uait.pro/concorp/tocrm.php';
+
+  const request = new XMLHttpRequest();
+  request.open('POST', url);
+  request.send(form_data);
+  console.log(`request ${request}`);
+  // refs.sucses.classList.toggle('active');
+  setTimeout(() => {
+    // Will run last, after 2000 milliseconds
+    console.log('send ', form_data);
+    // refs.sucses.classList.toggle('active');
+  }, 1500);
+  localStorage.clear();
+  // if (readData) {
+  //   // readData.email="asdas";
+  //   data.name = readData.name;
+  //   data.phone = readData.phone;
+  //   data.email = readData.email;
+  // } else {
+  //   data.name = '';
+  //   data.phone = '';
+  //   data.email = '';
+  // }
+  // refs.formain.name.value = data.name;
+  // refs.formain.phone.value = data.phone;
+  // refs.formain.email.value = data.email;
+  // saveLocal();
+  readLocal();
+  // saveLocal();
 }
