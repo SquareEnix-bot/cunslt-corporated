f8ddecdcb86fc2f3d8c8f3d940b4b7ee7d8eba02
Anastasiia Vozniuk <a.vozniuk.97@gmail.com>
2022-08-17 20:54:19 +0300

    fadeIn/fadeOut

--- src/js/accordion-services.js
+++ src/js/accordion-services.js
@@ -9,10 +9,39 @@ if (mediaQuery.matches) {
       let panel = this.lastElementChild;
 
       if (panel.style.display === "block") {
-        panel.style.display = "none";
+        // panel.style.display = "none";
+        fadeOut(panel)
       } else {
-        panel.style.display = "block";
+        // panel.style.display = "block";
+        fadeIn(panel)
       }
     });
   }
+}
+
+function fadeOut(el) {
+  var opacity = 1;
+  var timer = setInterval(function () {
+    if (opacity <= 0.1) {
+      clearInterval(timer);
+      el.style.display = "none";
+    }
+
+    el.style.opacity = opacity;
+    opacity -= opacity * 0.1;
+  }, 10);
+}
+
+function fadeIn(el) {
+  var opacity = 0.01;
+  el.style.display = "block";
+
+  var timer = setInterval(function () {
+    if (opacity >= 1) {
+      clearInterval(timer);
+    }
+
+    el.style.opacity = opacity;
+    opacity += opacity * 0.1;
+  }, 10);
 }
